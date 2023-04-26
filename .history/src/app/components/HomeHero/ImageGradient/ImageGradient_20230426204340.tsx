9a6cd4c8d1804dcb6a59d1e37fd34e8aaabfb6a7
SimonSkoda13 <simonskoda1354@gmail.com>
2023-04-26 20:45:57 +0200

    change welcome hore filter

--- .history/src/app/components/HomeHero/ImageGradient/ImageGradient_20230426204340.tsx
+++ .history/src/app/components/HomeHero/ImageGradient/ImageGradient_20230426204340.tsx
@@ -9,7 +9,7 @@ export const ImageGradient = (props: ImageGradientProps) => {
   return (
     <div className="relative w-full h-full overflow-hidden z-10">
       {/* <div className="absolute inset-0 z-10 bg-gradient-to-b from-secondary"></div> */}
-      <div className="absolute inset-0 z-10 bg-gray-900 opacity-30"></div>
+      <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
       {children}
     </div>
   );
